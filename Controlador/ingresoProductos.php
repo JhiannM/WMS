e66a43e16fc34e824f6719f595668c19e8a0e343
JhiannM <Jhiannmacias@gmail.com>
2025-11-30 16:24:07 -0500

    Implementación HU-003: registro de ingreso de productos

---
+++ Controlador/ingresoProductos.php
@@ -0,0 +1,90 @@
+<?php
+
+require_once '../Modelo/registrar.php';
+class ConsultarArticulos
+{
+    private $registrar;
+
+    public function __construct()
+    {
+        $this->registrar = new Registrar();
+    }
+
+    /* Obtener los articulos */
+    public function obtenerArticulos()
+    {
+        $busqueda = isset($_POST['search']) ? $_POST['search'] : '';
+
+        // Llamar al método de la clase Registrar para obtener los artículos
+        $articulos = $this->registrar->consultarArticulos($busqueda);
+
+        if ($articulos) {
+            echo json_encode([
+                'success' => true,
+                'data' => $articulos
+            ]);
+        } else {
+            echo json_encode([
+                'success' => false,
+                'mensaje' => 'No se encontraron artículos'
+            ]);
+        }
+    }
+
+    /* Obtener los proyectos */
+    public function obtenerProyectos()
+    {
+        // Llamar al método de la clase Registrar para obtener los proyectos
+        $proyectos = $this->registrar->consultarProyectos();
+
+        if ($proyectos) {
+            echo json_encode([
+                'success' => true,
+                'data' => $proyectos
+            ]);
+        } else {
+            echo json_encode([
+                'success' => false,
+                'mensaje' => 'No se encontraron proyectos'
+            ]);
+        }
+    }
+
+    /* Obtener los jefes */
+    public function obtenerJefes()
+    {
+        // Llamar al método de la clase Registrar para obtener los jefes
+        $jefes = $this->registrar->consultarJefeAprueba();
+
+        if ($jefes) {
+            echo json_encode([
+                'success' => true,
+                'data' => $jefes
+            ]);
+        } else {
+            echo json_encode([
+                'success' => false,
+                'mensaje' => 'No se encontraron jefes'
+            ]);
+        }
+    }
+
+}
+
+/* Ejecutador de funciones */
+if (isset($_GET['action']) && $_GET['action'] === 'articulos') {
+    $controller = new ConsultarArticulos();
+    // Puedes pasar $_POST directamente si lo deseas
+    $controller->obtenerArticulos($_POST);
+} else if (isset($_GET['action']) && $_GET['action'] === 'proyectos') {
+    $controller = new ConsultarArticulos();
+    $controller->obtenerProyectos();
+} else if (isset($_GET['action']) && $_GET['action'] === 'jefes') {
+    $controller = new ConsultarArticulos();
+    $controller->obtenerJefes();
+} else {
+    echo json_encode([
+        'success' => false,
+        'mensaje' => 'Acción no válida'
+    ]);
+}
